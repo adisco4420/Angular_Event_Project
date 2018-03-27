9af78a8f9b1e7fc54679b160d00539642e1b7b5c
adisco4420@gmail.com <adisco4420@gmail.com>
2018-03-27 11:46:55 +0100

    creating model-driven form

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -3,18 +3,21 @@ import {IUser} from './user.model'
 @Injectable()
 export class AuthService{
     currentUser:IUser
-    loginUser(userName: string, passord: string){
+    loginUser(userName: string, password: string){
         this.currentUser = {
             id:1,
             userName: userName,
-            firstName: 'Sodiq',
+            firstName: 'johne',
             lastName: 'Alabi'
-
         }
 
        
     }
     isAuthenticated(){
         return !!this.currentUser
+    } 
+    updateCurrentUser(firstName:string, lastName: string){
+        this.currentUser.firstName = firstName
+        this.currentUser.lastName = lastName
     }
 }
