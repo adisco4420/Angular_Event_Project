0d02efea581e9684ee52cd234275e01d4cb2d5fe
adisco4420@gmail.com <adisco4420@gmail.com>
2018-03-28 18:13:44 +0100

    sorting data

--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -9,6 +9,7 @@ import { ISession, restrictedWords } from '../../shared/index';
 export class SessionListComponent implements OnInit, OnChanges {
   @Input() sessions:ISession[]
   @Input() filterBy: string;
+  @Input() sortBy: string
   visibleSessions:ISession[] = [];
   constructor() { }
 
@@ -17,8 +18,11 @@ export class SessionListComponent implements OnInit, OnChanges {
 ngOnChanges(){
   if(this.sessions){
     this.filterSession(this.filterBy)
+    this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc)
+         : this.visibleSessions.sort(sortByVotesDesc)
   }
 }
+
 filterSession(filter){
   if(filter === 'all'){
     this.visibleSessions = this.sessions.slice(0)
@@ -29,4 +33,12 @@ filterSession(filter){
   }
 }
 
+}
+function sortByNameAsc(s1: ISession, s2: ISession){
+  if(s1.name > s2.name) return 1
+  else if(s1.name === s2.name) return 0
+  else return -1
+}
+function sortByVotesDesc(s1: ISession, s2: ISession){
+  return s2.voters.length - s1.voters.length
 }
